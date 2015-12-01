2d31926227369925e2656da862861db1427e3060
Deep Mehta <deepmehta.it@live.com>
2015-12-01 15:35:20 -0800

    senti

--- app.js
+++ app.js
@@ -66,13 +66,14 @@ app.get('/getStateevents', function (req, res) {
 app.get('/getnegative/:eventname', function (req, res) {
 	//req.param()
 	console.log(req.params.eventname);
+	var eventnamesplit=
 	console.log("get state events");
 	var client = hbase({
 		host: 'localhost',
 		port: 28080
 	});
 	var conection = new hbase.Connection(client);
-	conection.get('https://localhost:28080/ec_ng/Vegfest Glasgow*',function(error,data,response){
+	conection.get('https://localhost:28080/ec_ng/'+req.params.eventname+'*',function(error,data,response){
 		if(error){
 			console.log('error');
 			res.json('{"message":"error"}');
