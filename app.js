8c6d818a55fc8cbbebd20ca1a0cd26428b7400be
Deep Mehta <deepmehta.it@live.com>
2015-12-01 13:45:06 -0800

    trending events

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ app.get('/getevents', function (req, res) {
   //res.json('Hello World!');
 });
 
+app.get('/getTrendingevents', function (req, res) {
+	console.log("get Trending events");
+	var client = hbase({
+		host: 'localhost',
+		port: 28080
+	});
+	var conection = new hbase.Connection(client);
+	conection.get('https://localhost:28080/TrendingEvents/*',function(error,data,response){
+		if(error){
+			console.log('error');
+			res.json('{"message":"error"}');
+		}
+		//console.log("data->"+util.inspect(data,false,null));
+		res.json(data);
+
+	})
+  //res.json('Hello World!');
+});
+
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
