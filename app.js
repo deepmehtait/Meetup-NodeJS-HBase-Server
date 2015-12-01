69a3a36ff605ba8b27b8f895d2dc67f6d242f5db
Deep Mehta <deepmehta.it@live.com>
2015-12-01 04:32:02 -0800

    updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/getevents', function (req, res) {
 		port: 28080
 	});
 	var conection = new hbase.Connection(client);
-	connection.get('https://localhost:28080/rsvp/1540204908/',function(error,data,response){
+	conection.get('https://localhost:28080/rsvp/1540204908/',function(error,data,response){
 		if(error){
 			console.log('error');
 			res.json('{"message":"error"}');
